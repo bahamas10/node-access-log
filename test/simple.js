c7cd87b066d976a31cb54bbbd6a3af3f8733cc9a
Dave Eddy <dave@daveeddy.com>
2013-09-13 00:54:02 -0400

    clean up test

--- test/simple.js
+++ test/simple.js
@@ -5,8 +5,7 @@ var accesslog = require('../');
 var host = '127.0.0.1';
 var port = 9127;
 
-var server = http.createServer(onrequest);
-server.listen(port, host, started);
+http.createServer(onrequest).listen(port, host, started);
 
 function onrequest(req, res) {
   accesslog(req, res);
